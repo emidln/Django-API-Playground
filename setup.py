60e1b4aece0c4a05449908aeffa74cd86ef94351
Brandon Adams <emidln@gmail.com>
2013-02-28 15:36:25 -0500

    Version 0.2-bja, with setup info changed

--- setup.py
+++ setup.py
@@ -2,14 +2,14 @@ from setuptools import setup
 
 setup(
     name='django-api-playground',
-    version='0.1.1',
+    version='0.2-bja',
     packages=['apiplayground', 'apiplayground.templatetags'],
     zip_safe = False,
     include_package_data=True,
-    url='http://github.com/fatiherikli/django-api-playground',
+    url='http://github.com/emidln/django-api-playground',
     license='BSD',
-    author='fatiherikli',
-    author_email='fatiherikli@gmail.com',
+    author='fatiherikli, emidln',
+    author_email='emidln@gmail.com',
     description='API Playground for RESTful APIs',
     classifiers=[
         "Development Status :: 3 - Alpha",
